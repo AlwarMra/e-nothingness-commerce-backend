ffc57f4310124a9cfa13e31c2803d46a2369a967
alwar <alwaromoreno@gmail.com>
2022-10-08 02:29:33 +0200

    Setting up server

---
+++ data.js
@@ -0,0 +1,41 @@
+const data = {
+  products: [
+    {
+      id: 12334,
+      name: 'Mainly nothing',
+      slug: 'mainly-nothing',
+      description:
+        "How could you describe nothingness? Our star product will give you a fresh start to unravel the mistery. Do not let yourself be discouraged, you're paying for the feeling!",
+      price: 3995,
+      vendor: 'No one',
+      countInStock: 30,
+      image:
+        'https://i0.wp.com/the-meraki.com/wp-content/uploads/2017/03/moment-of-nothingness-1.jpg?fit=500%2C529',
+    },
+    {
+      id: 45656,
+      name: 'Emptiness',
+      slug: 'emptiness',
+      description:
+        "Did you ever feel alone? That's perfect, now you can embrace that concept with cash",
+      price: 1495,
+      vendor: 'No one',
+      countInStock: 5,
+      image:
+        'https://www.creativefabrica.com/wp-content/uploads/2022/03/15/White-Cement-Wall-Texture-Graphics-27184482-1-580x387.jpg',
+    },
+    {
+      id: 890456,
+      name: 'Into the void',
+      slug: 'into-the-void',
+      description:
+        "Glide into nothingness through the rabbit hole. It was a good movie but this is not a really good product, that is why we are charging you enough to subvert money's meaning",
+      price: 4995,
+      vendor: 'No one',
+      countInStock: 0,
+      image:
+        'https://lh3.googleusercontent.com/9zE3StG50FuHs9PCo6kMTIhWKyIc3vJ2JqxPZEgaKc8WQ2vzAyWR4wqYYhMZ7zkhc0C893COYflGKt70Sb15e4gg7eI=w640-h400-e365-rj-sc0x00ffffff',
+    },
+  ],
+}
+export default data
---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import data from './data.js'
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products)
+})
+
+app.listen(port, () => {
+  console.log(`Server on port${port}`)
+})
