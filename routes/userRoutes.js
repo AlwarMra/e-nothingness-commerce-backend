e70c960ec949c4f7cce2246503ddaa65a3c464af
alwar <alwaromoreno@gmail.com>
2022-10-11 19:09:55 +0200

    Register root

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,17 +15,43 @@ userRouter.post('/signin', (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then(user => {
       if (user && bcrypt.compareSync(req.body.password, user.password)) {
-        const { _id, __v, ...signedUser } = user.toObject()
-        res.send({
-          ...signedUser,
-          id: user.toObject()._id,
-          token: generateToken(user.toObject()),
-        })
+        res.send(formatUser(user))
         return
       }
       res.status(401).send({ err: true, message: 'Invalid email or password' })
     })
     .catch(err => next(err))
 })
+
+userRouter.post('/register', (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user)
+        return res
+          .status(406)
+          .send({ err: true, message: 'Email is already in use' })
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password),
+      })
+      newUser.save().then(user => {
+        console.log(user)
+        res.send(formatUser(user))
+        return
+      })
+    })
+    .catch(err => next(err))
+})
+
+function formatUser(user) {
+  const { _id, __v, password, ...signedUser } = user.toObject()
+  return {
+    ...signedUser,
+    id: user.toObject()._id,
+    token: generateToken(user.toObject()),
+  }
+}
 
 export default userRouter
